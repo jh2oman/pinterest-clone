cf1e49393ba69f59efe04cd083f26c2e301e040a
Jacob Waterman <jacobrwaterman@gmail.com>
2014-11-23 17:33:33 -0600

    adding AWS to heroku

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'jbuilder', '~> 1.2'
 gem 'bootstrap-sass'
 gem 'devise', '~> 3.4.1'
 gem 'paperclip', '~> 3.0'
-
+gem 'aws-sdk', '~> 1.20.0'
 group :development, :test do
 gem 'sqlite3'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,14 @@ Pinteresting::Application.configure do
   #required for heroku
   #set to actual host
   config.action_mailer.default_url_options = { host: 'jh2oman-pinteresting.com' }
+
+#required for AWS sdk
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
