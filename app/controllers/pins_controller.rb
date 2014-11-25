211924fa7d9c5dcce090d286b2265fbd26c9432c
Jacob Waterman <jacobrwaterman@gmail.com>
2014-11-24 21:03:15 -0600

    hello

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,5 @@
 class PinsController < ApplicationController
-  #before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :set_pin, only: [:show, :edit, :update, :destroy]
   #before_action :correct_user, only: [:edit, :update, :destroy]
   #before_action :authenticate_user!, except: [:index, :show]
   respond_to :html
