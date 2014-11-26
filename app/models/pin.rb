0aac1d8b4efea57e40ef6525bb675573c2d867e5
Jacob Waterman <jacobrwaterman@gmail.com>
2014-11-26 13:12:52 -0600

    added names

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,5 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  validates :image, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         has_many :pins
+  has_many :pins
+  validates :name, presence: true, length: {maximum: 70}
 end
