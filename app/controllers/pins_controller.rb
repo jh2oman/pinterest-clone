1ba56104fb29ed8bca4a609371196b1b915d5e18
Jacob Waterman <jacobrwaterman@gmail.com>
2014-11-26 23:31:13 -0600

    done

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -5,7 +5,7 @@ class PinsController < ApplicationController
   respond_to :html
 
   def index
-    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
+    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 15)
     #respond_with(@pins)
   end
 
